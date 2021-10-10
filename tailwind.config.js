911cd2e694acb53a1c50a8ee7d174f61e553da39
Stackbit Code Editor <projects@stackbit.com>
2021-10-10 10:31:09 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
         plugin(function ({ addBase, theme }) {
             addBase({
                 'h1': { fontSize: "50px" },
-                'h2': { fontSize: "100px" },
+                'h2': { fontSize: "120px" },
                 'h3': { fontSize: theme('fontSize.lg') },
             })
         })
