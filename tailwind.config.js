abe0e340be3ab31522251c98f1b03a65ac6f61bc
Stackbit Code Editor <projects@stackbit.com>
2021-10-11 15:06:18 +0300

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary: themeStyle.primary1
+                primary: "#cccccc"
             }
 
         }
